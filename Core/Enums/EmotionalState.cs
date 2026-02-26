638629fbfc60fb2d618819e7a0e38c06089acac9
Diego <diego.grajeda.7e8@itb.cat>
2026-02-26 12:03:42 +0100

    Core folder created with Enums and Interfaces folders and files

---
+++ Core/Enums/EmotionalState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi_Game.Core.Enums
+{
+    public enum EmotionalState
+    {
+        Happy,
+        Sad,
+        Angry,
+        Tired,
+        Sick
+    }
+}
